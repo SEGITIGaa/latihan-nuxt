1f553afc20467716243fe884ac58b82d14bc21e7
SEGITIGaa <ilyasjuandaraya2256@gmail.com>
2024-04-19 00:52:01 +0700

    pake uselazyfetch

---
+++ components/ongoing.vue
@@ -0,0 +1,27 @@
+<script setup>
+const { pending, data: ongoing } = await useLazyFetch('https://web-anime-psi.vercel.app/anime?type=ongoing');
+</script>
+<template>
+    <div class="w-full items-center justify-between flex">
+        <h1 class="h1">Ongoing</h1>
+        <NuxtLink to="/" class="arrow-btn">more</NuxtLink>
+    </div>
+
+    <div v-if="ongoing" class="carousel mb-10">
+        <NuxtLink :to="`/anime/${anime.slug}`" v-for="anime in ongoing" :key="anime.id" class="card">
+            <img :src="anime.gambar" :alt="anime.judul" class="card-img" />
+            <div class="flex flex-col gap-2 items-center w-56">
+                <p class="text-second font-semibold break-words w-52 truncate text-center text-lg">
+                    {{ anime.judul }}
+                </p>
+                <p class="text-second break-words truncate text-center text-xs">
+                    {{ anime.eps[1] }} episode
+                </p>
+            </div>
+        </NuxtLink>
+    </div>
+
+    <div v-if="pending" class="">
+        <p>bentar mas...</p>
+    </div>
+</template>
